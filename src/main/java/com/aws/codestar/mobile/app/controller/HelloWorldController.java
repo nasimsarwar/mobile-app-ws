d391cdb222821a21431213c2ea12666a2f125836
nasimsarwar <wdigilabs@gmail.com>
2021-03-03 09:45:46 +0530

    Added index home page and user Controller

--- src/main/java/com/aws/codestar/mobile/app/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/mobile/app/controller/HelloWorldController.java
@@ -2,6 +2,9 @@ package com.aws.codestar.mobile.app.controller;
 
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,23 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Basic Spring web service controller that handles all GET requests.
  */
-@RestController
-@RequestMapping("/")
+//@RestController
+//@RequestMapping("/")
+@Controller("/")
 public class HelloWorldController {
 
-    private static final String MESSAGE_FORMAT = "Mobile e %s!";
-
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "Web-Serive") String name) {
-        return ResponseEntity.ok(createResponse(name));
-    }
-
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity helloWorldPost(@RequestParam(value = "name", defaultValue = "Web-Serive") String name) {
-        return ResponseEntity.ok(createResponse(name));
-    }
-
-    private String createResponse(String name) {
-        return new JSONObject().put("Output", String.format(MESSAGE_FORMAT, name)).toString();
-    }
+	/*
+	 * private static final String MESSAGE_FORMAT = "Mobile e %s!";
+	 * 
+	 * @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	 * public ResponseEntity helloWorldGet(@RequestParam(value = "name",
+	 * defaultValue = "Web-Serive") String name) { return
+	 * ResponseEntity.ok(createResponse(name)); }
+	 * 
+	 * @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+	 * public ResponseEntity helloWorldPost(@RequestParam(value = "name",
+	 * defaultValue = "Web-Serive") String name) { return
+	 * ResponseEntity.ok(createResponse(name)); }
+	 * 
+	 * private String createResponse(String name) { return new
+	 * JSONObject().put("Output", String.format(MESSAGE_FORMAT, name)).toString(); }
+	 */
+    @GetMapping
+	public String indexPage(Model model) {
+    	//model.addAllAttributes(null)
+		return "index";
+	}
 }
---
+++ src/main/java/com/aws/codestar/mobile/app/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.aws.codestar.mobile.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+	@GetMapping
+	public String getUser() {
+		return "Hello Users";
+	}
+
+}
