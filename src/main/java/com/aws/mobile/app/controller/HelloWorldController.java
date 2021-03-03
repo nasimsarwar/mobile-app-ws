aed3693644298ff0398630a037bc67e5938ee733
nasimsarwar <wdigilabs@gmail.com>
2021-03-03 07:33:45 +0530

    Package Name Updated

--- src/main/java/com/aws/mobile/app/controller/HelloWorldController.java
+++ src/main/java/com/aws/mobile/app/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.aws.codestar.projecttemplates.controller;
+package com.aws.mobile.app.controller;
 
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloWorldController {
 
-    private static final String MESSAGE_FORMAT = "Hello %s!";
+    private static final String MESSAGE_FORMAT = "Mobile e %s!";
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public ResponseEntity helloWorldGet(@RequestParam(value = "name", defaultValue = "Web-Serive") String name) {
         return ResponseEntity.ok(createResponse(name));
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity helloWorldPost(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public ResponseEntity helloWorldPost(@RequestParam(value = "name", defaultValue = "Web-Serive") String name) {
         return ResponseEntity.ok(createResponse(name));
     }
 
